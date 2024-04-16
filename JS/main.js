fd2cb2d716aa194c7acb8183c08b3117a46415f0
uttam-salamander <uttamkumar8976@gmail.com>
2024-04-15 21:55:06 -0700

    Milestone Commit

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,91 @@
 let focusX = 0; 
 let focusY = 0;
-let focusRadius = 175;
+let focusRadius = 250;
+
+dataset_path = 'Data/Pokemon.csv';
+
+let xAttribute ;
+let yAttribute;
+let colorAttribute;
+
+const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Loading the dataset.
+
+    initPanel();
+
+    
+});
+
+d3.select("#x-attribute-select").on("change", init_graphs);
+d3.select("#y-attribute-select").on("change", init_graphs);
+d3.select("#color-select").on("change", init_graphs);
+
+function init_graphs(){
+
+    d3.select("#detailView").selectAll("*").remove();
+    d3.select("#miniChartContainer").selectAll("*").remove();
+    d3.select("#contextView").selectAll("*").remove();
+
+    xAttribute = document.getElementById("x-attribute-select").value;
+    yAttribute = document.getElementById("y-attribute-select").value;
+    colorAttribute = document.getElementById("color-select").value;
+
     miniMapPlotter();
     detailViewPlotter();
     contextPlotter();
 
-    // Make the chart draggable
-    const chartContainer = document.getElementById("miniChartContainer");
-    let isDragging = false;
-    let offset = { x: 0, y: 0 };
-
-    chartContainer.addEventListener("mousedown", startDrag);
-    chartContainer.addEventListener("mouseup", endDrag);
-    chartContainer.addEventListener("mousemove", drag);
-
-    function startDrag(event) {
-        isDragging = true;
-        offset.x = event.clientX - chartContainer.getBoundingClientRect().left;
-        offset.y = event.clientY - chartContainer.getBoundingClientRect().top;
-    }
-
-    function endDrag() {
-        isDragging = false;
-    }
-
-    function drag(event) {
-        if (isDragging) {
-            chartContainer.style.left = event.clientX - offset.x + "px";
-            chartContainer.style.top = event.clientY - offset.y + "px";
-            adjustFocusControl(); 
-            updateDetailView(); 
-        }
-    }
-
     initZoom();
-});
+}
+
+function initPanel() {
+
+     
+    d3.csv(dataset_path).then(data => {
+
+        attributes = Object.keys(data[0]);
+        //console.log('Attributes:', attributes);
+
+        attributesFiltered = {
+            quantitative : [],
+            categorical : []
+        }
+    
+        attributes.forEach(attribute => {
+
+            if(!(['#','Name', 'Type 2'].includes(attribute))){
+
+                const values = data.map(d => d[attribute]);
+                const isQuantitative = values.every(value => !isNaN(value));
+                if (isQuantitative) {
+                    attributesFiltered.quantitative.push(attribute);
+                } else {
+                    attributesFiltered.categorical.push(attribute);
+                }
+            }
+        });
+    
+        populateSelectOptions(d3.select("#x-attribute-select"), attributesFiltered.quantitative);
+        populateSelectOptions(d3.select("#y-attribute-select"), attributesFiltered.quantitative);
+        populateSelectOptions(d3.select("#color-select"), attributesFiltered.categorical);
+
+        init_graphs();
+
+
+    }).catch(error => {
+        console.error('Error loading CSV file:', error);
+    });
+        
+}
+
+function populateSelectOptions(selectElement, options) {
+    selectElement.selectAll("option").remove(); 
+    options.forEach(option => {
+        selectElement.append("option")
+            .attr("value", option)
+            .text(option);
+    });
+}
 
 let zoom = d3.zoom()
             .scaleExtent([0.25, 10])
@@ -107,7 +155,7 @@ function detailViewPlotter() {
     svg.append("circle")
         .attr("cx", width / 2)
         .attr("cy", height / 2)
-        .attr("r", 175) 
+        .attr("r", 250) 
         .attr("fill", "none")
         .attr("stroke", "black")
         .attr("stroke-width", 2);
@@ -133,50 +181,84 @@ function detailViewPlotter() {
 
     mask.append("circle")
         .attr("cx", width / 2)
-        .attr("cy", height / 2)
-        .attr("r", 175)
+        .attr("cy", height / 2 )
+        .attr("r", 250)
         .attr("fill", "black");
 
     // Read the data
-    d3.csv("consumption-co2-per-capita.csv").then(function(data) {
-        const groups = ["Asia (excl. China and India)", "United States", "Europe", "India", "China"]
+    d3.csv(dataset_path).then(function(data) {
 
-        data.forEach(function(d) {
-            d.year = new Date(+d.Year, 0, 1);
-            d.co2_emission = +d.Per_capita_consumption_based_CO2_emissions;
-            d.country = +d.Entity;
-        });
+        // Compute minimum and maximum values for x and y axes
+        const xMin = d3.min(data, d => parseFloat(d[xAttribute])) - 10;
+        const xMax = d3.max(data, d => parseFloat(d[xAttribute])) + 10;
+        const yMin = d3.min(data, d => parseFloat(d[yAttribute])) - 10;
+        const yMax = d3.max(data, d => parseFloat(d[yAttribute])) + 10;
 
-        // Set up scales
-        const xScale = d3.scaleTime()
-            .domain(d3.extent(data, d => d.year))
-            .range([0, width]);
+        console.log(xMax);
 
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.co2_emission)])
-            .range([height, 0]);
+        xScale = d3.scaleLinear()
+                        .domain([xMin, xMax])
+                        .range([0, width]);
 
-        // Color scale
-        var color = d3.scaleOrdinal(d3.schemeCategory10);
+        yScale = d3.scaleLinear()
+                        .domain([yMin, yMax])
+                        .range([height, 0]);
 
-        // Draw circles
-        svg_g.selectAll("circle")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("cx", d => xScale(d.year))
-            .attr("cy", d => yScale(d.co2_emission))
-            .attr("r", 3)
-            .attr("fill", d => color(d.Entity))
-            .attr("clip-path", "url(#circleMask)");
+        // Add data points
+        const circles = svg_g.selectAll(".scatter-point")
+                        .data(data)
+                        .join(
+                            enter => enter.append("circle")
+                                        .attr("class", "scatter-point")
+                                        .attr("id", (d, i) => "point-" + i)
+                                        .attr("cx", 
+                                        d => xScale(d[xAttribute]))
+                                        .attr("cy", 
+                                        d => yScale(d[yAttribute]))
+                                        .attr("r", 4)
+                                        .style("fill", d => colorScale(d[colorAttribute]))
+                                        .style('opacity', 0)
+                                        .attr("clip-path", "url(#circleMask)")
+                                        .call(enter => enter.transition().duration(1000)
+                                            .style('opacity', 1)),
 
-        // Add axes
+                            update => update.call(update => update.transition().duration(1000)
+                            .attr("cx", d => xScale(d[xAttribute]))
+                            .attr("cy", d => yScale(d[yAttribute]))
+                            .style("fill", '#000'))
+                        
+                        );
+
+        circles
+
+        // Add x-axis
         svg_g.append("g")
-            .attr("transform", `translate(0, ${height})`)
+            .attr("transform", "translate(0," + height + ")")
+            .transition().duration(750)
             .call(d3.axisBottom(xScale));
 
+        svg_g.append("text")
+            .attr("class", "x-axis-title")
+            .attr("x", width / 2)
+            .attr("y", height + margin.top + 20)
+            .style("text-anchor", "middle")
+            .text(xAttribute);
+            
+        // Add y-axis
         svg_g.append("g")
+            .transition().duration(750)
             .call(d3.axisLeft(yScale));
+
+        svg_g.append("text")
+            .attr("class", "y-axis-title")
+            .attr("transform", "rotate(-90)")
+            .attr("x", 0 - (height / 2))
+            .attr("y", 0 - margin.left)
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text(yAttribute);
+            
+
     });
 }
 
@@ -187,7 +269,7 @@ function miniMapPlotter() {
         width = 400 - margin.left - margin.right,
         height = 300 - margin.top - margin.bottom;
 
-    var svg = d3.select("#myDataVis")
+    var svg = d3.select("#miniChartContainer")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -205,35 +287,77 @@ function miniMapPlotter() {
         .attr("stroke-width", 2);
 
     // Read the data
-    d3.csv("consumption-co2-per-capita.csv").then(function(data) {
-        const xScale = d3.scaleTime()
-            .domain(d3.extent(data, d => new Date(+d.Year, 0, 1)))
-            .range([0, width]);
+    d3.csv(dataset_path).then(function(data) {
+        // Compute minimum and maximum values for x and y axes
+        const xMin = d3.min(data, d => parseFloat(d[xAttribute])) - 10;
+        const xMax = d3.max(data, d => parseFloat(d[xAttribute])) + 10;
+        const yMin = d3.min(data, d => parseFloat(d[yAttribute])) - 10;
+        const yMax = d3.max(data, d => parseFloat(d[yAttribute])) + 10;
 
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(data, d => +d.Per_capita_consumption_based_CO2_emissions)])
-            .range([height, 0]);
+        console.log(xMax);
 
-        // Color scale
-        var color = d3.scaleOrdinal(d3.schemeCategory10);
+        xScale = d3.scaleLinear()
+                        .domain([xMin, xMax])
+                        .range([0, width]);
 
-        // Add dots
-        svg.selectAll("circle")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("cx", d => xScale(new Date(+d.Year, 0, 1)))
-            .attr("cy", d => yScale(+d.Per_capita_consumption_based_CO2_emissions))
-            .attr("r", 2)
-            .attr("fill", d => color(d.Entity)); 
+        yScale = d3.scaleLinear()
+                        .domain([yMin, yMax])
+                        .range([height, 0]);
 
-        // Add axes
+        // Add data points
+        const circles = svg.selectAll(".scatter-point")
+                        .data(data)
+                        .join(
+                            enter => enter.append("circle")
+                                        .attr("class", "scatter-point")
+                                        .attr("id", (d, i) => "point-" + i)
+                                        .attr("cx", 
+                                        d => xScale(d[xAttribute]))
+                                        .attr("cy", 
+                                        d => yScale(d[yAttribute]))
+                                        .attr("r", 2)
+                                        .style("fill", d => colorScale(d[colorAttribute]))
+                                        .style('opacity', 0)
+                                        .call(enter => enter.transition().duration(1000)
+                                            .style('opacity', 1)),
+
+                            update => update.call(update => update.transition().duration(1000)
+                            .attr("cx", d => xScale(d[xAttribute]))
+                            .attr("cy", d => yScale(d[yAttribute]))
+                            .style("fill", '#000'))
+                        
+                        );
+
+        circles
+
+        // Add x-axis
         svg.append("g")
-            .attr("transform", `translate(0, ${height})`)
+            .attr("transform", "translate(0," + height + ")")
+            .transition().duration(750)
             .call(d3.axisBottom(xScale));
 
+        svg.append("text")
+            .attr("class", "x-axis-title")
+            .attr("x", width / 2)
+            .attr("y", height + margin.top + 20)
+            .style("text-anchor", "middle")
+            .text(xAttribute);
+            
+        // Add y-axis
         svg.append("g")
+            .transition().duration(750)
             .call(d3.axisLeft(yScale));
+
+        svg.append("text")
+            .attr("class", "y-axis-title")
+            .attr("transform", "rotate(-90)")
+            .attr("x", 0 - (height / 2))
+            .attr("y", 0 - margin.left)
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text(yAttribute);
+            
+        
     });
 
     svg.append('rect')
@@ -248,44 +372,51 @@ function miniMapPlotter() {
 }
 
 function contextPlotter() {
-    var contextRadius = 10 * focusRadius; 
-    var contextResolution = 10;
-    var barWidth = 10; 
+    // set the dimensions and margins of the graph
 
-    var svg = d3.select("#detailView svg");
+    var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = 700-margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom,
+    innerRadius = 250,
+    outerRadius = 300;   // the outerRadius goes from the middle of the SVG area to the border
 
-    var contextData = [];
-    var angleIncrement = 360 / contextResolution;
-    for (var i = 0; i < contextResolution; i++) {
-        var startAngle = i * angleIncrement;
-        var endAngle = (i + 1) * angleIncrement;
-        contextData.push({
-            startAngle: startAngle,
-            endAngle: endAngle,
-            innerRadius: focusRadius,
-            outerRadius: contextRadius,
-            value: 0
-        });
-    }
+    // append the svg object to the body of the page
+    var svg = d3.select("#contextView")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + ( height/2+100 )+ ")"); // Add 100 on Y translation, cause upper bars are longer
 
-    svg.selectAll(".contextBar")
-        .data(contextData)
+    d3.csv('Data/test_data.csv').then(function(data) {
+
+        // X scale
+        var x = d3.scaleBand()
+        .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+        .align(0)                  // This does nothing ?
+        .domain( data.map(function(d) { return d.Country; }) ); // The domain of the X axis is the list of states.
+
+        // Y scale
+        var y = d3.scaleRadial()
+        .range([innerRadius, outerRadius])   // Domain will be define later.
+        .domain([0, 10000]); // Domain of Y is from 0 to the max seen in the data
+
+        // Add bars
+        svg.append("g")
+        .selectAll("path")
+        .data(data)
         .enter()
-        .append("rect")
-        .attr("class", "contextBar")
-        .attr("x", function(d) {
-            return focusX + (d.outerRadius * Math.cos(toRadians(d.startAngle))) - focusRadius;
-        })
-        .attr("y", function(d) {
-            return focusY + (d.outerRadius * Math.sin(toRadians(d.startAngle))) - focusRadius;
-        })
-        .attr("width", barWidth)
-        .attr("height", function(d) {
-            return d.value; 
-        })
-        .attr("fill", "blue")
-        .attr("stroke", "black")
-        .attr("opacity", 0.5);
+        .append("path")
+        .attr("fill", "#69b3a2")
+        .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+            .innerRadius(innerRadius)
+            .outerRadius(function(d) { return y(d['Value']); })
+            .startAngle(function(d) { return x(d.Country); })
+            .endAngle(function(d) { return x(d.Country) + x.bandwidth(); })
+            .padAngle(0.01)
+            .padRadius(innerRadius))
+
+    });
 }
 
 
